49a546eef099d04373a922528ccdc9fb091f65d1
Rajan Kumar <raorajan9576@gmail.com>
2024-03-30 12:02:49 +0530

    constant file created

---
+++ constant/gmail.constant.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const auth = {
+    type: 'OAuth2',
+    user: 'raorajan9576@gmail.com',
+    clientId: process.env.CLIENT_ID as string,
+    clientSecret: process.env.CLIENT_SECRET as string,
+    refreshToken: process.env.REFRESH_TOKEN as string,
+};
+
+const mailOptions = {    
+    to: 'shrikishunr7@gmail.com',
+    from: 'raorajan9576@gmail.com',
+    subject: 'Gmail API using Node JS',
+};
+
+export default {
+    auth,
+    mailOptions
+};
+
