450e491d51f62b568fb00e1dfe7c2cfd98cb133e
Rajan Kumar <raorajan9576@gmail.com>
2024-03-31 16:36:53 +0530

    try to get sender email for send email by bullmq

--- src/controller/gmail.controller.ts
+++ src/controller/gmail.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import nodemailer from 'nodemailer';
-import { google } from 'googleapis';
-import dotenv from 'dotenv';
-import EmailModel from '../model/user.model';
-import { createConfig } from '../utils/gmail.utils';
+import { Request, Response } from "express";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+import EmailModel from "../model/user.model";
+import { createConfig } from "../utils/gmail.utils";
 
 dotenv.config();
 
@@ -16,17 +16,17 @@ interface ReceivedEmail {
 }
 
 const auth = {
-  type: 'OAuth2',
-  user: 'raorajan9576@gmail.com',
+  type: "OAuth2",
+  user: "raorajan9576@gmail.com",
   clientId: process.env.CLIENT_ID as string,
   clientSecret: process.env.CLIENT_SECRET as string,
   refreshToken: process.env.REFRESH_TOKEN as string,
 };
 
 const mailOptionsBase = {
-  to: 'shrikishunr7@gmail.com',
-  from: 'raorajan9576@gmail.com',
-  subject: 'Gmail API using Node JS',
+  to: "shrikishunr7@gmail.com",
+  from: "raorajan9576@gmail.com",
+  subject: "Gmail API using Node JS",
 };
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -43,13 +43,13 @@ async function sendMail(req: Request, res: Response): Promise<void> {
   try {
     const accessToken = await oAuth2Client.getAccessToken();
     if (!accessToken) {
-      throw new Error('Access token is null or undefined');
+      throw new Error("Access token is null or undefined");
     }
     const token = accessToken.token as string;
     const transport = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
-        type: 'OAuth2',
+        type: "OAuth2",
         user: auth.user,
         clientId: auth.clientId,
         clientSecret: auth.clientSecret,
@@ -60,7 +60,7 @@ async function sendMail(req: Request, res: Response): Promise<void> {
 
     const mailOptions: nodemailer.SendMailOptions = {
       ...mailOptionsBase,
-      text: 'This is a test mail using Gmail API',
+      text: "This is a test mail using Gmail API",
     };
 
     const result = await transport.sendMail(mailOptions);
@@ -75,8 +75,8 @@ async function sendMail(req: Request, res: Response): Promise<void> {
 
     res.send(result);
   } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).send('Error sending email');
+    console.error("Error sending email:", error);
+    res.status(500).send("Error sending email");
   }
 }
 
@@ -84,24 +84,26 @@ async function getMails(req: Request, res: Response): Promise<void> {
   try {
     const email = req.params.email as string;
     if (!email) {
-      throw new Error('Email address is missing');
+      throw new Error("Email address is missing");
     }
 
     const url = `https://gmail.googleapis.com/gmail/v1/users/${email}/threads?maxResults=100`;
     const accessToken = await oAuth2Client.getAccessToken();
     if (!accessToken) {
-      throw new Error('Access token is null or undefined');
+      throw new Error("Access token is null or undefined");
     }
     const token = accessToken.token as string;
     const config: AxiosRequestConfig = createConfig(url, token);
     const response: AxiosResponse = await axios(config);
 
-    const emails: ReceivedEmail[] = response.data.threads.map((thread: any) => ({
-      email,
-      threadId: thread.id,
-      snippet: thread.snippet,
-      receivedAt: new Date(),
-    }));
+    const emails: ReceivedEmail[] = response.data.threads.map((thread: any) => {
+      return {
+        email,
+        threadId: thread.id,
+        snippet: thread.snippet,
+        receivedAt: new Date(),
+      };
+    });
 
     for (const email of emails) {
       await EmailModel.storeReceivedEmail(email);
@@ -109,8 +111,8 @@ async function getMails(req: Request, res: Response): Promise<void> {
 
     res.json(emails);
   } catch (error) {
-    console.error('Error fetching emails:', error);
-    res.status(500).send('Error fetching emails');
+    console.error("Error fetching emails:", error);
+    res.status(500).send("Error fetching emails");
   }
 }
 
